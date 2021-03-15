9c1d5fec740710883fa6aeba122212dff5b25973
jtorres97 <torresj1997@outlook.com>
2021-03-15 15:31:21 -0500

    Added InfoBox component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import './App.css';
+import InfoBox from './components/InfoBox';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
   useEffect(() => {
     const getData = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
@@ -19,17 +22,27 @@ function App() {
     getData();
   }, []);
 
+  const onCountryChange = (e) => {
+    setCountry(e.target.value);
+  };
+
   return (
     <div className="app">
       <div className="appHeader">
         <h1>COVID-19 Dashboard</h1>
         <FormControl className="appDropdown">
-          <Select variant="outlined" value="US">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
+      </div>
+      <div className="appStats">
+        <InfoBox title="Coronavirus Cases" cases={8000} total={2000} />
+        <InfoBox title="Recovered" cases={10000} total={300} />
+        <InfoBox title="Deaths" cases={50000} total={900} />
       </div>
     </div>
   );
---
+++ src/components/InfoBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+const InfoBox = ({ title, cases, total }) => {
+  return (
+    <Card className="infoBox">
+      <CardContent>
+        <Typography className="infoBoxTitle" color="textSecondary">
+          {title}
+        </Typography>
+        <h2 className="infoBoxCases">{cases}</h2>
+        <Typography className="infoBoxTotal" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default InfoBox;
