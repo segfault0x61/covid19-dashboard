53dbc8e7c2669bfeddefcb11b0b941ea51fec8ad
jtorres97 <torresj1997@outlook.com>
2021-03-15 09:12:47 -0500

    Install Material UI, add a dropdown for countries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([
+    'Worldwide',
+    'USA',
+    'China',
+    'UK',
+  ]);
   return (
     <div className="app">
-      <h1>COVID19 Dashboard</h1>
+      <div className="appHeader">
+        <h1>COVID-19 Dashboard</h1>
+        <FormControl className="appDropdown">
+          <Select variant="outlined" value="dummy">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
