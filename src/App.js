a70185383d88744d2707395d38caeaba3501cf57
jtorres97 <torresj1997@outlook.com>
2021-03-15 09:29:21 -0500

    Fill countries dropdown with external API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import { FormControl, MenuItem, Select } from '@material-ui/core';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [countries, setCountries] = useState([
-    'Worldwide',
-    'USA',
-    'China',
-    'UK',
-  ]);
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((item) => ({
+            name: item.country,
+            value: item.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getData();
+  }, []);
+
   return (
     <div className="app">
       <div className="appHeader">
         <h1>COVID-19 Dashboard</h1>
         <FormControl className="appDropdown">
-          <Select variant="outlined" value="dummy">
+          <Select variant="outlined" value="US">
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
